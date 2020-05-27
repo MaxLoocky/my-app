ac65ec45d2a3456c4aa7c70b48d01074cb5a0757
MaxLoocky <Yuality@yandex.ru>
2020-05-27 11:37:16 +0300

    #3 merge mastr -> dev

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './main/Header';
+import Navbar from './main/Navbar';
+import Content from './main/Content';
+import Footer from './main/Footer';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Navbar />
+      <Content />
+      <Footer />
     </div>
   );
 }
